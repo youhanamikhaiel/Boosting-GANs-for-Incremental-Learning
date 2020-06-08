9083380f3f3471fa8089e64d127b39f7e6e065c6
Youhana Mikhaiel <youhana.mikhaiel1@gmail.com>
2020-06-08 12:29:03 +0200

    Update train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -26,18 +26,17 @@ def main(config):
     """
         
     global resnet_real_feats, dist
-    dist = [ torch.Tensor([]).to('cuda') for _ in range(10) ]
+    dist = [torch.Tensor([]) for _ in range(config['n_classes'])]
     
     #load real data for distance computation
     b_size = 50000
     normalize_real = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616])
     transform_real = transforms.Compose([normalize_real])
     trainset_real = GANDataset( 'samples/real_data/CIFAR10_training.npz', 'samples/real_data/CIFAR10_weights.npz', transform=transform_real)
-    trainloader_real = torch.utils.data.DataLoader(trainset_real, batch_size=b_size, shuffle=False, num_workers=0, pin_memory=True)
+    trainloader_real = torch.utils.data.DataLoader(trainset_real, batch_size=b_size, shuffle=False, num_workers=8, pin_memory=True)
     
     resnet_real_feats, indices = dist_utils.get_real_feats(trainloader_real,'resnet20')
-    
-    persample_weights = dist_utils.get_sample_weights(config)
+    persample_weights = dist_utils.get_sample_weights(resnet_real_feats,indices,config)
     
     
     """
