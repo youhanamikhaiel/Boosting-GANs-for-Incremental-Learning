d4a33241dfe871bab7269723eed789a4b581fe77
Youhana Mikhaiel <youhana.mikhaiel1@gmail.com>
2020-06-10 11:08:00 +0200

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -79,10 +79,16 @@ def main():
   gan_weight_file = 'BigGAN_C10_seed0_Gch96_Dch96_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema'
 	
   # Loading configuration
-  if gan_weight_file[23] == '9':
-    config = params.params96
-  elif gan_weight_file[23] == '6':
-    config = params.params64
+  if gan_weight_file[0] == 'B':
+    if gan_weight_file[20] == '9':
+      config = params.params96
+    elif gan_weight_file[20] == '6':
+      config = params.params64
+  else:
+    if gan_weight_file[23] == '9':
+      config = params.params96
+    elif gan_weight_file[23] == '6':
+      config = params.params64
 	
   utils.update_config(config)
 	
